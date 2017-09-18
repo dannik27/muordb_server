a760e8312289e1d62652f08f4692f786b108053b
Danil Nikiforovykh <nikifordan@gmail.com>
2017-09-18 22:56:09 +0300

    gson + json output

--- src/main/java/com/muordb/muordb_server/controller/GuestController.java
+++ src/main/java/com/muordb/muordb_server/controller/GuestController.java
@@ -1,10 +1,14 @@
 package com.muordb.muordb_server.controller;
 
+import com.muordb.muordb_server.entity.DemoEntity;
 import com.muordb.muordb_server.service.DemoEntityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
 
 @Controller
 public class GuestController{
@@ -17,12 +21,20 @@ public class GuestController{
         return "index";
     }
 
-    @RequestMapping("/demodata")
+    @RequestMapping("/demodata/show")
     public ModelAndView demoData(){
         ModelAndView mav = new ModelAndView();
         mav.addObject("data", demoEntityService.getDemoData());
         mav.setViewName("demodata");
         return mav;
     }
+
+    @RequestMapping("/demodata/list")
+    @ResponseBody
+    public ArrayList<DemoEntity> demoDataList(){
+        return demoEntityService.getDemoData();
+    }
+
+
 
 }
